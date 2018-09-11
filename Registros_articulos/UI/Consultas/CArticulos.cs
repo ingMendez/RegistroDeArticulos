ffba44e8eb850507ced3bbf78921e06bb867ca89
Reydi Alejandro Mendez Suarez <raydi_mendez@ucne.edu.do>
2018-09-11 10:48:58 -0400

    creando los formularios

---
+++ Registros_articulos/UI/Consultas/CArticulos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Registros_articulos.UI.Consultas
+{
+    public partial class CArticulos : Form
+    {
+        public CArticulos()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Registros_articulos/UI/Registros/rArticulos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Registros_articulos.UI.Registros
+{
+    public partial class rArticulos : Form
+    {
+        public rArticulos()
+        {
+            InitializeComponent();
+        }
+    }
+}
