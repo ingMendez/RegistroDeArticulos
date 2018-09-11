a4bd4bbecb13b5baadc6645f8b985f67a5fc28ce
Reydi Alejandro Mendez Suarez <raydi_mendez@ucne.edu.do>
2018-09-11 09:38:07 -0400

    haciendo clase contexto

--- Registros_articulos/BLL/ArticulosBLL.cs
+++ Registros_articulos/BLL/ArticulosBLL.cs
@@ -5,12 +5,24 @@ using System.Linq;
 using System.Text;
 
 namespace Registros_articulos.BLL
+
+    ////
+    ///aqui van todas las operaciones a realizar
+    ///
 {
      public class ArticulosBLL
     {
+        /// <summary>
+        ///  aqui vamos aguardar en la entidad de la base de datos
+        /// </summary>
+        /// <param name="articulos"></param>
+        /// <returns></returns>
         public static bool Guardar(Articulos articulos)
         {
-
+            bool paso = false;
+            // nuestra variable que nos dara acceso a la base de dato
+            Contexto contexto = new Contexto();
+            
 
             return false;
         }
---
+++ Registros_articulos/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Registros_articulos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace Registros_articulos.DAL
+{
+    public class Contexto:DbContext
+    {
+        public DbSet<Articulos> articulos { get; set;}
+
+        public Contexto() : base("ConStr")
+        {
+
+        }
+    }
+}
