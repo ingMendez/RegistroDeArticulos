5243a18b9054f8cfa58f86baedb5642b08b1edcd
Reydi Alejandro Mendez Suarez <raydi_mendez@ucne.edu.do>
2018-09-11 08:55:32 -0400

    Comensado proyecto

---
+++ Registros_articulos/DAL/Scripts/Scripts.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE ArticulosDB
+go 
+use ArticulosDB
+go
+CREATE TABLE Articulos
+(
+ArticuloId int primary key identity,
+FechaVencimiento datetime,
+Descripcion varchar(50),
+Precio int,
+Existencia int,
+CantCotizada int
+);
